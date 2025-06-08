be39dccb2ca96abb3b22449cb39056bf548c9247
Ramon Pacheco <rmpacheco2@gmail.com>
2025-06-08 11:21:12 -0300

    update authentication

--- authentication/src/main/kotlin/com/authentication/app/service/AuthenticationService.kt
+++ authentication/src/main/kotlin/com/authentication/app/service/AuthenticationService.kt
@@ -1,16 +1,19 @@
 package com.authentication.app.service
 
 import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class AuthenticationService {
+
     companion object {
-        private const val INVALID_CARD: String = "INVALID_CARD"
+        private const val INVALID_USER: String = "INVALID_USER";
     }
 
-    fun authenticate(creditCard: String) {
-        if (creditCard == INVALID_CARD) {
-            throw IllegalArgumentException("Invalid credit card")
+    fun authenticate(creditCard: String): String {
+        if (creditCard == INVALID_USER) {
+            throw IllegalArgumentException("Invalid User")
         }
+        return "TOKEN: " + UUID.randomUUID().toString()
     }
 }
